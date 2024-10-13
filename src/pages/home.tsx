fdef2b74617bda2b7ab2272403911d082c365a6d
Evan Mitchell <etmitchell2022@gmail.com>
2024-10-12 23:14:35 -0400

    Add bio

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -11,6 +11,18 @@ const Home = () => {
           <div className='flex items-end h-full p-2'>
             <IntroHeaderAnimation />
           </div>
+          <div className='mt-8 min-h-full p-2'>
+            <p className='text-white text-lg leading-8 font-sans font-thin'>
+              I'm a software engineer specializing in web development and
+              frontend infrastructure. My passion is improving Developer
+              Experience (DX) by creating tools to make development easier.
+            </p>
+          </div>
+          <div className='mt-4 min-h-full p-2'>
+            <h1 className='text-white text-2xl leading-8 font-sans font-thin'>
+              Work
+            </h1>
+          </div>
         </div>
       </div>
     </div>
