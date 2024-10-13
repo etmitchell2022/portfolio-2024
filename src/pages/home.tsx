997b9860a47b86ef25b8783ff57751320ae96cf6
Evan Mitchell <etmitchell2022@gmail.com>
2024-10-12 23:59:44 -0400

    Add magic card

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,7 +7,7 @@ const Home = () => {
     <div className='min-w-full min-h-screen bg-custom-bg flex justify-center'>
       <div className=' w-[48rem] relative'>
         <CustomMap />
-        <div className='absolute top-36 left-0 w-full h-24 bg-custom-gradient pointer-events-none'>
+        <div className='absolute top-36 left-0 w-full h-24 bg-custom-gradient'>
           <div className='flex items-end h-full p-2'>
             <IntroHeaderAnimation />
           </div>
