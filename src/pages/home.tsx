124d96541e5244986bdebabaaaba65d89089d206
Evan Mitchell <54292815+etmitchell2022@users.noreply.github.com>
2024-11-28 22:56:34 -0500

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -32,25 +32,43 @@ const Home = () => {
             </p>
           </div>
           <div className="mt-4 min-h-full p-2">
-            <h1 className="text-white text-3xl leading-8 font-sans font-thin">
-              Work
-            </h1>
-            <div className="flex items-center mt-4">
-              <AnimatedDTLogo size={20} />
-              <div className="ml-3">
-                <h1 className="text-white text-xl font-sans mb-1">
-                  DeveloperTown
-                </h1>
-                <div className="flex items-center text-gray-400 text-sm space-x-4">
-                  <div className="flex items-center">
-                    <IoBriefcaseOutline className="mr-2" />
-                    <span>Software Engineer</span>
-                  </div>
-                  <div className="flex items-center">
-                    <FaRegCalendar className="mr-2" />
-                    <span>January 2022 - Present</span>
+            <div>
+              <h1 className="text-white text-3xl leading-8 font-sans font-thin">
+                Work
+              </h1>
+              <div className="flex items-center mt-4">
+                <AnimatedDTLogo size={20} />
+                <div className="ml-3">
+                  <h1 className="text-white text-xl font-sans mb-1">
+                    DeveloperTown
+                  </h1>
+                  <div className="flex items-center text-gray-400 text-sm space-x-4">
+                    <div className="flex items-center">
+                      <IoBriefcaseOutline className="mr-2" />
+                      <span>Software Engineer</span>
+                    </div>
+                    <div className="flex items-center">
+                      <FaRegCalendar className="mr-2" />
+                      <span>January 2022 - Present</span>
+                    </div>
                   </div>
                 </div>
+              </div>
+              <div className="py-2">
+                <p className="text-gray-200 text-sm leading-8 font-sans font">
+                  At{" "}
+                  <span className="text-dt-blue  font-semibold">
+                    DeveloperTown
+                  </span>
+                  , I have developed and delivered mission-critical software
+                  across multiple industries. This includes building an app for
+                  a major airline that is used on 1,000+ flights daily by pilots
+                  and flight attendants, as well as creating a platform in the
+                  commercial real estate space to provide clients with insights
+                  into their data and connectors, handling billions of data
+                  points.Along the way, I've deepened my expertise across many
+                  technologies, to solve complex, real-world problems.
+                </p>
               </div>
             </div>
             <div className="flex items-center mt-4">
