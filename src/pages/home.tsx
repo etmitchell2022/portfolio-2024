3f540f17c7518e32ac2d5b20abc6b5c5128a0615
Evan Mitchell <54292815+etmitchell2022@users.noreply.github.com>
2024-11-28 22:20:51 -0500

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,12 +6,16 @@ import portraitImage from "../assets/images/portrait.jpg";
 import { FaRegCalendar } from "react-icons/fa";
 import { IoBriefcaseOutline, IoSchoolOutline } from "react-icons/io5";
 import { AnimatedBSULogo } from "@/components/animations/AnimatedBSULogo";
+import Clock from "@/components/clock";
 
 const Home = () => {
   return (
     <div className="min-w-full min-h-screen bg-custom-bg flex justify-center">
       <div className=" w-[48rem] relative">
         <CustomMap />
+        <div className="p-2 absolute top-0 left-0">
+          <Clock />
+        </div>
         <div className="absolute top-36 left-0 w-full h-24 bg-custom-gradient">
           <div className="flex items-end justify-between h-full p-2">
             <IntroHeaderAnimation />
