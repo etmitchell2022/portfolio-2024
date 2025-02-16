e966eaef18d110c4236b4e521437810cb11e0716
Evan Mitchell <etmitchell2022@gmail.com>
2025-02-16 01:04:44 -0500

    update

---
+++ src/components/reconize-logo.tsx
@@ -0,0 +1,123 @@
+export const ReconizeLogo = ({ size = 100 }) => {
+  return (
+    <svg
+      width={size}
+      height={size * (754 / 584)}
+      viewBox='0 0 135 143'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M85.9 118.94L68.36 93.61H49V118.94H28V28H67.36C91.65 28 106.85 40.6 106.85 61C106.85 74.64 99.97 84.64 88.14 89.71L108.54 118.94H85.9ZM66.15 45.15H49V76.85H66.15C79.01 76.85 85.51 70.85 85.51 61C85.51 51.15 79.01 45.15 66.15 45.15Z'
+        fill='#F6F6F6'
+      />
+      <path
+        d='M68 12H109.79C112.413 12 115.009 12.5179 117.432 13.5239C119.854 14.53 122.053 16.0045 123.904 17.8627C125.755 19.7209 127.22 21.9263 128.217 24.3524C129.213 26.7784 129.72 29.3773 129.71 32V65.87'
+        stroke='#DF031D'
+        stroke-width='9'
+        stroke-miterlimit='10'
+      />
+      <path
+        d='M67.71 137.83H25.92C23.3041 137.83 20.7137 137.315 18.2969 136.314C15.8801 135.313 13.6842 133.845 11.8344 131.996C9.98469 130.146 8.51739 127.95 7.51632 125.533C6.51525 123.116 6 120.526 6 117.91V84'
+        stroke='#DF031D'
+        stroke-width='9'
+        stroke-miterlimit='10'
+      />
+      <path
+        d='M40.0448 29.8418C41.1116 23.2698 36.6487 17.0773 30.0766 16.0105C23.5045 14.9437 17.312 19.4066 16.2452 25.9786C15.1784 32.5507 19.6413 38.7432 26.2134 39.81C32.7854 40.8768 38.978 36.4139 40.0448 29.8418Z'
+        fill='#FF172E'
+      />
+      <rect
+        x='20.3034'
+        y='20.1495'
+        width='15.7794'
+        height='15.7156'
+        fill='url(#pattern0_119_2)'
+      />
+      <rect
+        x='21.6183'
+        y='20.8329'
+        width='13.1495'
+        height='14.349'
+        fill='url(#pattern1_119_2)'
+      />
+      <g filter='url(#filter0_if_119_2)'>
+        <path
+          d='M40.0448 29.9552C41.1246 23.303 36.6073 17.035 29.9552 15.9552C23.303 14.8754 17.035 19.3927 15.9552 26.0449C14.8754 32.697 19.3927 38.9651 26.0449 40.0449C32.697 41.1247 38.965 36.6074 40.0448 29.9552Z'
+          fill='#FF172E'
+        />
+      </g>
+      <defs>
+        <pattern
+          id='pattern0_119_2'
+          patternContentUnits='objectBoundingBox'
+          width='1'
+          height='1'
+        >
+          <use
+            transform='scale(0.027027 0.0277778)'
+          />
+        </pattern>
+        <pattern
+          id='pattern1_119_2'
+          patternContentUnits='objectBoundingBox'
+          width='1'
+          height='1'
+        >
+          <use
+            transform='scale(0.0322581 0.03125)'
+          />
+        </pattern>
+        <filter
+          id='filter0_if_119_2'
+          x='0.795654'
+          y='0.795654'
+          width='54.4087'
+          height='54.4087'
+          filterUnits='userSpaceOnUse'
+          color-interpolation-filters='sRGB'
+        >
+          <feFlood flood-opacity='0' result='BackgroundImageFix' />
+          <feBlend
+            mode='normal'
+            in='SourceGraphic'
+            in2='BackgroundImageFix'
+            result='shape'
+          />
+          <feColorMatrix
+            in='SourceAlpha'
+            type='matrix'
+            values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+            result='hardAlpha'
+          />
+          <feOffset dy='4' />
+          <feGaussianBlur stdDeviation='4.5' />
+          <feComposite in2='hardAlpha' operator='arithmetic' k2='-1' k3='1' />
+          <feColorMatrix
+            type='matrix'
+            values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0'
+          />
+          <feBlend
+            mode='normal'
+            in2='shape'
+            result='effect1_innerShadow_119_2'
+          />
+          <feGaussianBlur
+            stdDeviation='7.5'
+            result='effect2_foregroundBlur_119_2'
+          />
+        </filter>
+        <image
+          id='image0_119_2'
+          width='37'
+          height='36'
+        />
+        <image
+          id='image1_119_2'
+          width='31'
+          height='32'
+        />
+      </defs>
+    </svg>
+  );
+};
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,130 +1,203 @@
-import CustomMap from "../components/map";
-import IntroHeaderAnimation from "../components/animations/IntroHeaderAnimation";
-import AnimatedDTLogo from "@/components/animations/AnimatedDTLogo";
-import WiggleImage from "@/components/wiggle-image";
-import portraitImage from "../assets/images/portrait.jpg";
-import { FaRegCalendar } from "react-icons/fa";
-import { IoBriefcaseOutline, IoSchoolOutline } from "react-icons/io5";
-import { AnimatedBSULogo } from "@/components/animations/AnimatedBSULogo";
-import Clock from "@/components/clock";
-import CountUp from "react-countup";
+import CustomMap from '../components/map';
+import IntroHeaderAnimation from '../components/animations/IntroHeaderAnimation';
+import AnimatedDTLogo from '@/components/animations/AnimatedDTLogo';
+import WiggleImage from '@/components/wiggle-image';
+import portraitImage from '../assets/images/portrait.jpg';
+import { FaRegCalendar } from 'react-icons/fa';
+import { IoBriefcaseOutline, IoSchoolOutline } from 'react-icons/io5';
+import { GoDeviceCameraVideo } from 'react-icons/go';
+import { AnimatedBSULogo } from '@/components/animations/AnimatedBSULogo';
+import Clock from '@/components/clock';
+import CountUp from 'react-countup';
+import { ReconizeLogo } from '@/components/reconize-logo';
 
 const Home = () => {
   return (
-    <div className="min-w-full min-h-screen bg-custom-bg flex flex-col items-center">
+    <div className='min-w-full min-h-screen bg-custom-bg flex flex-col items-center'>
       {/* Sticky Map */}
-      <div className="max-w-[48rem] min-h-screen bg-custom-bg flex flex-col items-center">
-        <div className="sticky top-0 z-0 h-[220px] w-full">
+      <div className='max-w-[48rem] min-h-screen bg-custom-bg flex flex-col items-center'>
+        <div className='sticky top-0 z-0 h-[220px] w-full'>
           <CustomMap />
-          <div className="absolute top-2 left-2">
+          <div className='absolute top-2 left-2'>
             <Clock />
           </div>
         </div>
 
-        <div className="absolute top-36 left-0 w-full h-24 bg-custom-gradient z-10"></div>
+        <div className='absolute top-36 left-0 w-full h-24 bg-custom-gradient z-10'></div>
 
-        <div className="relative z-20 w-full bg-custom-bg">
-          <div className="w-full mx-auto px-2 py-8">
-            <div className="pb-8">
-              <div className="flex items-end justify-between -mt-[200px]">
+        <div className='relative z-20 w-full bg-custom-bg'>
+          <div className='w-full mx-auto px-2 py-8'>
+            <div className='pb-8'>
+              <div className='flex items-end justify-between -mt-[200px]'>
                 <IntroHeaderAnimation />
-                <div className="mr-2">
+                <div className='mr-2'>
                   <WiggleImage imageSrc={portraitImage} />
                 </div>
               </div>
-              <div className="mt-10">
-                <p className="text-white text-lg leading-8 font-sans font-thin">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-                  eveniet consequatur beatae labore dignissimos enim magni cum
-                  expedita reiciendis pariatur? Lorem ipsum, dolor sit amet
-                  consectetur adipisicing elit. Debitis.
+              <div className='mt-10'>
+                <p className='text-white text-lg leading-8 font-sans font-thin'>
+                  I'm a software engineer specializing in scalable web
+                  infrastructure, developer tooling, and automation
                 </p>
               </div>
             </div>
 
-            <div className="mt-8">
-              <h1 className="text-white text-3xl leading-8 font-sans font-thin">
+            <div className='mt-8'>
+              <h1 className='text-white text-3xl leading-8 font-sans font-thin'>
                 Work
               </h1>
-              <div className="flex items-center mt-4">
+              <div className='flex items-center mt-4'>
                 <AnimatedDTLogo size={20} />
-                <div className="ml-3">
-                  <h1 className="text-white text-xl font-sans mb-1">
+                <div className='ml-3'>
+                  <h1 className='text-white text-xl font-sans mb-1'>
                     DeveloperTown
                   </h1>
-                  <div className="flex items-center text-gray-400 text-sm space-x-4">
-                    <div className="flex items-center">
-                      <IoBriefcaseOutline className="mr-2" />
+                  <div className='flex items-center text-gray-400 text-sm space-x-4'>
+                    <div className='flex items-center'>
+                      <IoBriefcaseOutline className='mr-2' />
                       <span>Software Engineer</span>
                     </div>
-                    <div className="flex items-center">
-                      <FaRegCalendar className="mr-2" />
+                    <div className='flex items-center'>
+                      <FaRegCalendar className='mr-2' />
                       <span>January 2022 - Present</span>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="py-2">
-                <p className="text-gray-200 text-sm leading-8 font-sans">
-                  At{" "}
-                  <span className="text-dt-blue font-semibold">
+              <div className='py-2'>
+                <p className='text-gray-200 text-sm leading-8 font-sans'>
+                  At{' '}
+                  <span className='text-dt-blue font-semibold'>
                     DeveloperTown
                   </span>
                   , I have developed and delivered mission-critical software
                   across multiple industries. This includes building an app for
-                  a major airline that is used on{" "}
-                  <span>
+                  a major airline that is used on{' '}
+                  <span className='font-bold text-white'>
                     <CountUp end={1000} duration={1.5} />
                   </span>
                   + flights daily by pilots and flight attendants, as well as
                   creating a platform in the commercial real estate space to
                   provide clients with insights into their data and connectors,
-                  handling billions of data points. Along the way, I've deepened
-                  my expertise across many technologies, solving complex,
-                  real-world problems.
+                  handling{' '}
+                  <span className='font-bold text-white'>billions</span> of data
+                  points. Along the way, I've deepened my expertise across many
+                  technologies, solving complex, real-world problems.
                 </p>
               </div>
-              <div className="flex items-center mt-4">
+              <div className='flex items-center mt-4'>
                 <AnimatedDTLogo size={20} />
-                <div className="ml-3">
-                  <h1 className="text-white text-xl font-sans mb-1">
+                <div className='ml-3'>
+                  <h1 className='text-white text-xl font-sans mb-1'>
                     DeveloperTown
                   </h1>
-                  <div className="flex items-center text-gray-400 text-sm space-x-4">
-                    <div className="flex items-center">
-                      <IoBriefcaseOutline className="mr-2" />
+                  <div className='flex items-center text-gray-400 text-sm space-x-4'>
+                    <div className='flex items-center'>
+                      <IoBriefcaseOutline className='mr-2' />
                       <span>Software Engineer Intern</span>
                     </div>
-                    <div className="flex items-center">
-                      <FaRegCalendar className="mr-2" />
+                    <div className='flex items-center'>
+                      <FaRegCalendar className='mr-2' />
                       <span>May 2021 - August 2021</span>
                     </div>
                   </div>
                 </div>
               </div>
+              <div className='py-2'>
+                <p className='text-gray-200 text-sm leading-8 font-sans'>
+                  As an intern, I built a CLI tool to bridge the gap between
+                  design and the front end developement team. It took design
+                  tokens in Figma and converted them into Material UI theme
+                  overrides. This tool cut the setup process of new projects as
+                  the theme and components like buttons and inputs could be
+                  configured automatically. A tool like this allowed for faster
+                  development of key features and saved countless developer
+                  hours that would previously have been used.
+                </p>
+              </div>
             </div>
 
-            <div className="mt-8">
-              <h1 className="text-white text-3xl leading-8 font-sans font-thin">
+            <div className='mt-8'>
+              <h1 className='text-white text-3xl leading-8 font-sans font-thin'>
+                What I'm Currently building
+              </h1>
+              <div className='flex items-center mt-4'>
+                <ReconizeLogo size={35} />
+                <div className='ml-3'>
+                  <h1 className='text-white text-xl font-sans mb-1'>
+                    Reconize
+                  </h1>
+                  <div className='flex items-center text-gray-400 text-sm space-x-4'>
+                    <div className='flex items-center'>
+                      <GoDeviceCameraVideo className='mr-2' />
+                      <span>Private Investigation Software</span>
+                    </div>
+                    <div className='flex items-center'>
+                      <FaRegCalendar className='mr-2' />
+                      <span>September 2024 - Present</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='py-2'>
+                <p className='text-gray-200 text-sm leading-8 font-sans'>
+                  I'm building{' '}
+                  <span className='text-reconize-red font-semibold'>
+                    Reconize
+                  </span>
+                  , an AI-driven platform that transforms{' '}
+                  <span className='font-bold text-white'>hours</span> of raw
+                  surveillance footage into actionable insights in{' '}
+                  <span className='font-bold text-white'>minutes</span>.
+                  Traditionally, investigators spend days manually reviewing,
+                  editing, and compiling reports. Reconize automates this
+                  process, detecting key events, segmenting relevant footage,
+                  and generating structured reports instantly. This drastically
+                  improves efficiency, reduces workload, and enhances
+                  investigative accuracy—making video analysis faster, smarter,
+                  and more effective.
+                </p>
+              </div>
+            </div>
+
+            <div className='mt-8'>
+              <h1 className='text-white text-3xl leading-8 font-sans font-thin'>
                 School
               </h1>
-              <div className="flex items-center mt-4">
+              <div className='flex items-center mt-4'>
                 <AnimatedBSULogo size={30} />
-                <div className="ml-3">
-                  <h1 className="text-white text-xl font-sans mb-1">
+                <div className='ml-3'>
+                  <h1 className='text-white text-xl font-sans mb-1'>
                     Ball State University
                   </h1>
-                  <div className="flex items-center text-gray-400 text-sm space-x-4">
-                    <div className="flex items-center">
-                      <IoSchoolOutline className="mr-2" />
+                  <div className='flex items-center text-gray-400 text-sm space-x-4'>
+                    <div className='flex items-center'>
+                      <IoSchoolOutline className='mr-2' />
                       <span>Computer Science</span>
                     </div>
-                    <div className="flex items-center">
-                      <FaRegCalendar className="mr-2" />
+                    <div className='flex items-center'>
+                      <FaRegCalendar className='mr-2' />
                       <span>August 2018 - December 2021</span>
                     </div>
                   </div>
                 </div>
+              </div>
+              <div className='py-2'>
+                <p className='text-gray-200 text-sm leading-8 font-sans'>
+                  Senior Project: Built a web application for Ball State to
+                  provide a no-cost alternative for obtaining regalia (caps,
+                  gowns, hoods) for university events. The project was adopted
+                  by Ball State faculty starting in Fall 2021.
+                </p>
+
+                <p className='text-gray-400 text-sm leading-8 font-sans'>
+                  Awards: Dean's List Spring 2019, Fall 2019, Spring 2020, Fall
+                  2020, Spring 2021, Fall 2021
+                </p>
+                <p className='text-gray-400 text-sm leading-8 font-sans'>
+                  Treasurer: Association of Information Technology Professionals
+                  (AITP)
+                </p>
               </div>
             </div>
           </div>
