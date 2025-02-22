f276feb342aea0d471743d6d293f8354fc1b5505
Evan Mitchell <etmitchell2022@gmail.com>
2025-02-22 00:36:58 -0500

    update

--- src/components/ui/industry-card.tsx
+++ src/components/ui/industry-card.tsx
@@ -9,30 +9,34 @@ import {
 } from './card';
 import { Button } from './button';
 import { BorderBeam } from './border-beam';
+import PlaneSpiral from '../plane-spiral';
 
 const IndustryCard = () => {
   return (
-    <Card className='relative w-[350px] overflow-hidden bg-[#29292e] border-none'>
-      <CardHeader>
-        <CardTitle>Airline</CardTitle>
-      </CardHeader>
-      <CardContent>
-        
-      </CardContent>
-      <BorderBeam
-        duration={8}
-        size={100}
-        colorFrom='#ffffff'
-        colorTo='#50B4E5'
-      />
-      <BorderBeam
-        duration={8}
-        delay={4}
-        size={100}
-        colorFrom='#ffffff'
-        colorTo='#50B4E5'
-      />
-    </Card>
+<Card className="relative w-[300px] h-[250px] overflow-hidden bg-[#29292e] border-none rounded-xl">
+  <CardHeader>
+    <CardTitle>Airline Industry</CardTitle>
+  </CardHeader>
+  <CardContent></CardContent>
+
+  <div className="absolute bottom-[-60px] left-1/2 transform -translate-x-1/2 w-[300px] h-[170px] flex justify-center overflow-hidden">
+    <PlaneSpiral />
+  </div>
+
+  <BorderBeam
+    duration={8}
+    size={100}
+    colorFrom="#ffffff"
+    colorTo="#50B4E5"
+  />
+  <BorderBeam
+    duration={8}
+    delay={4}
+    size={100}
+    colorFrom="#ffffff"
+    colorTo="#50B4E5"
+  />
+</Card>
   );
 };
 
